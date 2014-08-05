8c6990343f12b4f05764190ac7f255bf1c7f2314
Yerena <alejo.yerena@gmail.com>
2014-08-05 11:07:55 -0500

    prueba

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	try{
-		$conn = new PDO("mysql:host=localhost;dbname=db_empresa","root","root");
-		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
-	}catch(PDOException $pdoe){
-		echo $pdoe->getMessage();
-	}
-
-?>
