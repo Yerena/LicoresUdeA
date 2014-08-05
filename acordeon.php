2adbcfeb37f9c351fb1ab13da61ac8f00e960bee
Yerena <alejo.yerena@gmail.com>
2014-08-05 09:04:59 -0500

    primer comentario

---
+++ acordeon.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="css/acordeon_style.css"/>
+	</head>
+	<body>
+<?php
+	try{	
+		require_once __DIR__ . "/db.php";
+		$state = $conn->prepare("SELECT * FROM licor WHERE grados_alcohol >= 15");
+	   	$state->execute();
+		$state->setFetchMode(PDO::FETCH_ASSOC);
+		echo'	<div class="accordian">
+			<ul>';		
+		while($var = $state->fetch()){
+		echo'
+				<li>
+					<div class="image_title">
+						<a href="#">'.$var["nombre"].'</a>
+					</div>
+					<a href="#">
+						<img src = "'.$var["imagen"].'"/>
+					</a>
+				</li>';
+		}
+		echo'	</ul>
+		</div>';
+					$var = $state->fetch();
+	
+	}catch(PDOException $pdoe){
+		echo $pdoe->getMessage();
+	}
+?>
+	</body>
+</html>
