70f0f53fe4f65cf3b9f9418c50278c61e032d035
Yerena <alejo.yerena@gmail.com>
2014-08-05 18:36:27 -0500

    Se cambio user y pass de db

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$conn = new PDO("mysql:host=localhost;dbname=db_empresa","root","root");
+		$conn = new PDO("mysql:host=localhost;dbname=db_empresa","adminnLE36ed","7Z3m7IgHP-An");
 		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
 	}catch(PDOException $pdoe){
 		echo $pdoe->getMessage();
