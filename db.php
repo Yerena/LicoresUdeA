166fa91fdeb0f5122402d7e0f2668e2d2213b666
Yerena <alejo.yerena@gmail.com>
2014-08-05 11:06:53 -0500

    comiit

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$conn = new PDO("mysql:host=licoresudea-fundamentos.rhcloud.com;dbname=licoresudea","admin7mfjFFt","cfMjq7gekFl-");
+		$conn = new PDO("mysql:host=localhost;dbname=db_empresa","root","root");
 		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
 	}catch(PDOException $pdoe){
 		echo $pdoe->getMessage();
