35e420b7e7893a5fed64b404b50958edc992abb5
Yerena <alejo.yerena@gmail.com>
2014-08-06 17:19:01 -0500

    Se quita la conexión de la base de datos
    Ahora las imágnes no se traen desde la BD sino que están seteadas en un
    arreglo local llamado $ruta

--- vinos_cream.php
+++ vinos_cream.php
@@ -6,7 +6,31 @@
 	<body>
 <?php
 	include 'header.php';
-	try{	
+//	try{	
+		$root_path = "img/";
+		$ruta = array();
+		$ruta[0]= $root_path . "alba_crema.jpg";
+		$ruta[1]= $root_path . "club_colombia.jpg";
+		$ruta[2]=$root_path . "sauvignon_harlan.jpg";
+
+		echo'	<div class="accordian">
+			<ul>';	
+
+
+		for ($i=0; $i<count($ruta); $i++){
+			echo'
+				<li>
+					<div class="image_title">
+						<a href="#">Imagen</a>
+					</div>
+					<a href="#">
+						<img src = "'.$ruta[$i].'"/>
+					</a>
+				</li>';
+		}
+		echo'	</ul>
+		</div>';
+/*
 		require_once __DIR__ . "/db.php";
 		$state = $conn->prepare("SELECT * FROM licor WHERE (grados_alcohol >= 10 && grados_alcohol <= 20)");
 	   	$state->execute();
@@ -31,6 +55,10 @@
 	}catch(PDOException $pdoe){
 		echo $pdoe->getMessage();
 	}
+*/
 ?>
 	</body>
 </html>
+
+
+<img src="camino hacia el archivo">
