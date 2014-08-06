39177bacb6a22db75c6b7413499c80eab54bb343
Yerena <alejo.yerena@gmail.com>
2014-08-06 17:30:42 -0500

    NO BD

--- acordeon.php
+++ acordeon.php
@@ -5,6 +5,36 @@
 	</head>
 	<body>
 <?php
+
+
+
+		$root_path = "img/";
+		$ruta = array();
+		$ruta[0]= $root_path . "alba_crema.jpg";
+		$ruta[1]= $root_path . "club_colombia.jpg";
+		$ruta[2]=$root_path . "sauvignon_harlan.jpg";
+
+		echo'	<div class="accordian">
+			<ul>';	
+
+
+		for ($i=0; $i<count($ruta); $i++){
+			echo'
+				<li>
+					<div class="image_title">
+						<a href="#">Imagen</a>
+					</div>
+					<a href="#">
+						<img src = "'.$ruta[$i].'"/>
+					</a>
+				</li>';
+		}
+		echo'	</ul>
+		</div>';
+
+
+
+/*
 	try{	
 		require_once __DIR__ . "/db.php";
 		$state = $conn->prepare("SELECT * FROM licor WHERE grados_alcohol >= 15");
@@ -30,6 +60,7 @@
 	}catch(PDOException $pdoe){
 		echo $pdoe->getMessage();
 	}
+*/
 ?>
 	</body>
 </html>
