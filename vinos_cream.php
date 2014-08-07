88e61981f2e0fc09077cb0d4da317993f57fe10a
Yerena <alejo.yerena@gmail.com>
2014-08-06 19:38:42 -0500

    vinossss

--- vinos_cream.php
+++ vinos_cream.php
@@ -6,24 +6,41 @@
 	<body>
 <?php
 	include 'header.php';
-//	try{	
+
 		$root_path = "img/vinos/";
 		$ruta = array();
 		$ruta[0]= $root_path . "sauvignon_harlan.jpg";
 		$ruta[1]= $root_path . "diablo_cabernet.jpg";
 		$ruta[2]=$root_path . "cabernet_merlot.jpg";
 		$ruta[3]=$root_path . "vega_unico.jpg";
-		$ruta[4]=$root_path . "pinot_noir_botella.jpg";
+		
 
 		$nombre = array();
 		$nombre[0] = "Sauvignon Harlan";
 		$nombre[1] = "Diablo Cabernet";
 		$nombre[2] = "Cabernet Merlot";
 		$nombre[3] = "Vega Unico";
-		$nombre[4] = "Pinot Noir";
+		
 
 		echo'	<div class="accordian">
 			<ul>';	
+
+
+		for ($i=0; $i<count($ruta); $i++){
+			echo'
+				<li>
+					<div class="image_title">
+						<a href="#">'.$nombre[$i].'</a>
+					</div>
+					<a href="#">
+						<img src = "'.$ruta[$i].'"/>
+					</a>
+				</li>';
+		}
+		echo'	</ul>
+		</div>';
+
+		//	try{	
 /*
 		require_once __DIR__ . "/db.php";
 		$state = $conn->prepare("SELECT * FROM licor WHERE (grados_alcohol >= 10 && grados_alcohol <= 20)");
